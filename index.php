0c5a66f9f761c3293e8a2dbf44a25128d9944ca9
Dev404 <jamesoncherry243@gmail.com>
2023-12-21 09:51:47 +0100

    tp-quizz

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>TP-Quizz</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand">Quizz</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Mes Statistiques</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Utilisateurs Connectés</a>
+          </li>
+              <li>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        </form>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<div class="card text-center text-white bg-dark ">
+  <div class="card-header">
+   Inscprition
+  </div>
+  <div class="d-flex justify-content-center">
+  <div class="card-body card text-bg-dark mb-3" style="max-width: 30rem;">
+    <h5 class="card-title">Entrer votre pseudo</h5>
+    <div class="mb-3">
+      <input type="text" id="disabledTextInput" class="form-control">
+    </div>
+
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">S'inscrire</a>
+  </div>
+ 
+
+ 
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ questions.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Quizz</title>
+</head>
+<body>
+
+
+<div class="card text-center text-white bg-dark">
+  <div class="card-header">
+  <h1>Question 1 :</h1>
+  </div>
+  <div class="d-flex justify-content-center">
+  <div class="card-body card text-bg-dark mb-3" style="max-width: 30rem;">
+    <h5 class="card-title">Quel a été le dernier film Disney qui est sorti cette année ?</h5>
+    <div class="mb-3">
+      <input type="text" id="disabledTextInput" class="form-control">
+    </div>
+    
+    <div class="mb-3">
+      <input type="text" id="disabledTextInput" class="form-control">
+    </div>
+
+    <div class="mb-3">
+      <input type="text" id="disabledTextInput" class="form-control">
+    </div>
+
+    <div class="mb-3">
+      <input type="text" id="disabledTextInput" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary">Valider</button>
+
+
+  </fieldset>
+</form>
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+</html>
